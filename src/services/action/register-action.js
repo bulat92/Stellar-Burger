dc75ac0c677f265b4c3b1d243b000869cb618053
bulat92 <49103720+bulat92@users.noreply.github.com>
2022-07-14 19:26:14 +0500

    Смена расширения

--- src/services/action/register-action.js
+++ src/services/action/register-action.js
@@ -7,17 +7,11 @@ import {
 } from "./login-action";
 import { setCookie } from "../cookie/cookie-functions";
 
-export const REGISTER_FETCH_SUCCESS: = "REGISTER_SUCCESS",
+export const REGISTER_FETCH_SUCCESS = "REGISTER_SUCCESS",
   REGISTER_FETCH_REQUEST = "REGISTER_REQUEST",
   REGISTER_FETCH_FAILED = "REGISTER_FAILED";
 
-interface TRegisterFetch {
-  email: string; 
-  password: string; 
-  name: string;
-}
-
-export const registerFetch: TRegisterFetch = (email, password, name) => {
+export const registerFetch = (email, password, name) => {
   return function (dispatch) {
     dispatch({
       type: LOGIN_FETCH_REQUEST,
